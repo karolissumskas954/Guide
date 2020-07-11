cbbfb8a7fd0c4e197f74e5721a88b225fc25e012
Karl <nhl1323@gmail.com>
2020-07-11 10:39:36 +0300

    Listo kurimas ir elemento pasiekimas

--- hello.py
+++ hello.py
@@ -6,3 +6,10 @@ print("hello")
 
 # kaip sukuriamas exeption
 raise Exception("Kazkas ivyko (error)")
+# Kita variacija
+raise SyntaxError("Sintakses klaida")
+
+# Listo kurimas
+my_list = ["one"]
+# Listo elemento pasiekimas
+print(my_list[0])
